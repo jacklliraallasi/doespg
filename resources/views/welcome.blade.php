a1bff9499555a50a024e2b314b5d470c4cca4bdc
Jackson Lira A <jackliraallasi@gmail.com>
2018-07-14 17:44:31 -0500

    Modificacion de Archivo Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Jackson Lira
                 </div>
 
                 <div class="links">
